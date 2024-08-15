31be5f8bfcfda8e03f10281b4e759093a039097d
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-15 15:19:44 -0300

    first commit

---
+++ src/main/java/com/tidz/restTuto/RestTutoApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.restTuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidz/restTuto/RestTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.restTuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
